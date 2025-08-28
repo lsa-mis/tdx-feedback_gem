ca898b3f2114cdc2d0e3e6fbb6877a604221972e
rsmokeUM <rsmoke@umich.edu>
2025-08-28 10:49:29 -0400

    Update integration test to require spec_helper instead of rails_helper for improved compatibility

--- spec/integration/real_app_integration_spec.rb
+++ spec/integration/real_app_integration_spec.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require 'rails_helper'
+require 'spec_helper'
 
 # This spec is designed to be run against a real Rails application
 # to test the full integration of the gem
